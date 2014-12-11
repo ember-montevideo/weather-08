e280747b4392297137dc41e1787f4deb1f32adfd
Adrian Mugnolo <adrian@wyeworks.com>
2014-12-11 21:51:58 -0200

    Hit service

--- app/routes/cities/city.js
+++ app/routes/cities/city.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return params.city_id;
+    return Ember.$.ajax('https://query.yahooapis.com/v1/public/yql', {
+      data: {
+        q: 'select item from weather.forecast where woeid=' + params.city_id + ' and u="c"',
+        format: 'json'
+      }
+    }).then(function(response) {
+      return {
+        title: response.query.results.channel.item.title,
+        description: response.query.results.channel.item.description
+      };
+    });
   }
 });
